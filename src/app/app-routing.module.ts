bc01c963b1ef8a141a058af352afacf26cda0b5a
Matth998 <matheusoliveira.3125@gmail.com>
2022-05-06 23:57:39 -0300

    Consumo do backend completo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CorrentistasComponent } from './components/correntistas/correntistas.component';
+import { HomeComponent } from './components/home/home.component';
+import { MovimentacaoListComponent } from './components/movimentacao-list/movimentacao-list.component';
+import { MovimentacaoNewComponent } from './components/movimentacao-new/movimentacao-new.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+
+  {path: 'home', component: HomeComponent},
+  {path: 'movimentacao-new', component: MovimentacaoNewComponent},
+  {path: 'movimentacoes', component: MovimentacaoListComponent},
+  {path: 'correntistas', component: CorrentistasComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MovimentacaoListComponent } from './components/movimentacao-list/movimentacao-list.component';
+import { MovimentacaoNewComponent } from './components/movimentacao-new/movimentacao-new.component';
+import { HomeComponent } from './components/home/home.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { CurrencyMaskModule } from 'ng2-currency-mask';
+import { CorrentistasComponent } from './components/correntistas/correntistas.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovimentacaoListComponent,
+    MovimentacaoNewComponent,
+    HomeComponent,
+    CorrentistasComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    CurrencyMaskModule
   ],
   providers: [],
   bootstrap: [AppComponent]
